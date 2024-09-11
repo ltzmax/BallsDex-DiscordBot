1df9f5c74a0b0d143e5f119c6facc07f7da86b7e
MAX <63972751+ltzmax@users.noreply.github.com>
2024-09-11 23:28:10 +0200

    y

--- ballsdex/packages/countryballs/spawn.py
+++ ballsdex/packages/countryballs/spawn.py
@@ -12,7 +12,7 @@ from ballsdex.packages.countryballs.countryball import CountryBall
 
 log = logging.getLogger("ballsdex.packages.countryballs")
 
-SPAWN_CHANCE_RANGE = (10, 50)
+SPAWN_CHANCE_RANGE = (30, 50)
 
 CachedMessage = namedtuple("CachedMessage", ["content", "author_id"])
 
